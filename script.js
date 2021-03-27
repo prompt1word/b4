6422a3f3f061892ed3f7383fcbf6ee14ad47a399
Glitch (melted-dot-guanaco) <none>
2021-03-27 15:50:31 +0000

    🤗🍦 Updated with Glitch

--- script.js
+++ script.js
@@ -1,37 +1,5 @@
-// header scrolling effect
-$(window).on('scroll', function(){
-	if($(window).scrollTop()){
-      $('header').addClass('nav-show');
-		  
-	} 
-	else{
-		$('header').removeClass('nav-show');
-	}
-	   
-})
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
 
-//hamburger
-const navSlide = () => {
-	 const hamburger = document.querySelector(".hamburger");
-	 const navbar = document.querySelector(".nav-bar");
-	 const navLinks = document.querySelectorAll(".nav-bar li");
-
-     hamburger.onclick = () => {
-		
-	 navbar.classList.toggle("nav-active");
-		 
-      //Animation links
-	 navLinks.forEach((link, index) => {
-		if (link.style.animation) {
-			link.style.animation = "";
-		} else {
-			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
-		   }
-		});
-	  //hamburger animation
-	 hamburger.classList.toggle("toggle");
-    }
-	 
-	}
-
-window.onload = () => navSlide();
+// prints "hi" in the browser's dev tools console
+console.log("hi");
